9ad44c5189a2140d1c470e09bd7fdd9c1ad57b42
Michael Darmousseh <michael@milyoni.com>
2013-11-18 15:57:24 -0800

    move defaults to manifest to maintain lightweight embed code

--- public/javascripts/manifest.js
+++ public/javascripts/manifest.js
@@ -1,4 +1,21 @@
+var trackingRegex = window.location.href.match(/(t=|tracking_id=)([^&]*)/);
+var trackingId = trackingRegex && trackingRegex[2];
+var userAgent = navigator.userAgent;
+var appVersion = function() {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+   return 'mobile';
+  } else {
+   return 'desktop';
+  }
+};
+
+//Set default data here
+defaultData = {
+  tracking_id: trackingId, user_agent: userAgent, app_version: appVersion
+};
+
+//Define interaction custom data below.
 manifest = {
   "BLC": {category: "button", object: "hello_link", action: "click", data: {destination: 'yourFace'}},
-  "PGV": {category: "page", object: "gallery", action: "visit", data: {tracking_id: '12345'}}
+  "PGV": {category: "page", object: "gallery", action: "visit"}
 };
