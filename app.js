5d110695ce1536c91e3057cb65806888204559cc
Michael Darmousseh <michael@milyoni.com>
2013-11-20 10:30:41 -0800

    add cross domain headers

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ app.use(express.cookieParser());
 app.use(express.session({secret: 'blahblah'}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin',      req.headers.origin);
+  res.header('Access-Control-Allow-Methods',     'GET,POST');
+  res.header('Access-Control-Allow-Headers',     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 app.use(ipinfo);
 
 // development only
