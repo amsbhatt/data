d0bc0c522a20f81d9ab1bec6455770608429059d
Michael Darmousseh <michael@milyoni.com>
2013-11-20 11:03:46 -0800

    changed to allow all origins

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin',      req.headers.origin);
+  res.header('Access-Control-Allow-Origin',      '*');
   res.header('Access-Control-Allow-Methods',     'GET,POST');
   res.header('Access-Control-Allow-Headers',     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
