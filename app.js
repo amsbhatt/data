438a4525d4f7e868eea7d0104b05561582e04dbb
Michael Darmousseh <michael@milyoni.com>
2013-11-20 14:16:45 -0800

    move to top

--- app.js
+++ app.js
@@ -19,24 +19,26 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('ipinfo', ipinfo);
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin',      '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods',     'GET,POST');
+  res.header('Access-Control-Allow-Headers',     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(ipinfo);
 app.use(express.cookieParser());
 app.use(express.session({secret: 'blahblah'}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin',      '*');
-  res.header('Access-Control-Allow-Methods',     'GET,POST');
-  res.header('Access-Control-Allow-Headers',     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  next();
-});
-app.use(ipinfo);
+
+
 
 // development only
 if ('development' == app.get('env')) {
