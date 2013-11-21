a80d88927e4615065b468a2a6ceeefeeba170884
Michael Darmousseh <michael@milyoni.com>
2013-11-20 16:58:00 -0800

    oops

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.post('/interactions', function(req, res) {
-  console.info('req present', req)
+  console.info('req present', req.session)
   sessionStorage.currentSession.create(req, function(response) {
     console.info('session request', req.session)
     //listen to response for session_id before creating interactions
