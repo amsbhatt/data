5522e7a5ca451b4660e4d8156d6e07a9d5eaf05b
Michael Darmousseh <michael@milyoni.com>
2013-11-20 16:25:46 -0800

    debug

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('ipinfo', ipinfo);
 app.use(function(req, res, next) {
-  console.info('req', req)
+  console.info('req header origin', req.headers.origin)
   if (req) {
     res.header('Access-Control-Allow-Origin',      req.headers.origin);
     res.header('Access-Control-Allow-Credentials', true);
