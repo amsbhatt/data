2be89fae5626468e6055befabb494d1e37e7a982
Michael Darmousseh <michael@milyoni.com>
2013-11-20 17:48:58 -0800

    it turns out the apostrophes were not nasty at all

--- models/session.js
+++ models/session.js
@@ -28,7 +28,7 @@ exports.currentSession = {
       }
       var dataHash = hstore.stringify(data);
       // Check to see if the session already exists
-      client.query("SELECT id from sessions where key=" + sessionId + ";", function(err, response) {
+      client.query("SELECT id from sessions where key='" + sessionId + "';", function(err, response) {
         if (err) {
           return console.error('error querying id', err);
         }
@@ -38,7 +38,7 @@ exports.currentSession = {
         }
         // if session does not exist, write to db, and return id to callback
         if (!(response && !!(response.rows[0] && response.rows[0].id))) {
-          client.query("INSERT INTO sessions (key, data) VALUES (" + sessionId + ",'" + dataHash + "') RETURNING id;", function(err, res) {
+          client.query("INSERT INTO sessions (key, data) VALUES ('" + sessionId + "','" + dataHash + "') RETURNING id;", function(err, res) {
             done();
             if (err) {
               return console.error("error inserting session", err);
