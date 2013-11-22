ed27bb423bb5f60f8b06b8ad5da86d36cce23512
Michael Darmousseh <michael@milyoni.com>
2013-11-22 14:45:06 -0800

    add more notes

--- public/javascripts/manifest.js
+++ public/javascripts/manifest.js
@@ -5,6 +5,10 @@
 * Three-Five letter acronym that includes the category, object, action and if destination is present include in acronym
 * For example, category: 'button', object: 'image', action: 'click', data: {destination: 'Tumblr'} would have an acronym of
 *   BICT ('B' for button, 'I' for image, 'C' for click, 'T' for tumblr
+*
+* To trigger manual interactions - make a call to DNA.interaction(DNA.manifest[insert_identifier here])
+* For example, a call to PGV would be DNA.interaction(DNA.manifest['PGV'])
+*
 * */
 
 //Define interaction custom data below.
