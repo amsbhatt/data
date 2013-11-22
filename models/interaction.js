870cd97c59202ff11291dd64a647706640521778
Michael Darmousseh <michael@milyoni.com>
2013-11-22 14:32:40 -0800

    fixed

--- models/interaction.js
+++ models/interaction.js
@@ -35,7 +35,6 @@ var validParams = function(req) {
 
 //Default data
 var defaultData = function(urlPath) {
-  console.info('url Path!!!!!!!!!!!!!!!!', urlPath)
   var trackingRegex = urlPath.match(/(t=|tracking_id=)([^&]*)/);
   var trackingId = trackingRegex && trackingRegex[2];
 
@@ -55,10 +54,8 @@ exports.create = function(sessionId, req, res) {
     };
 
     var data = req.body;
-    console.info('data before!!!!!!!!!!!!!!!', data)
     //Get default info about user's browser
     var defaults = defaultData(data.browserUrl);
-    console.info('defaults data!!!!!!!!!!!!!!', defaults)
     delete data.browserUrl;
 
     if (validParams(req)) {
--- public/javascripts/manifest.js
+++ public/javascripts/manifest.js
@@ -1,5 +1,14 @@
+//Allowed options for category are: button, page
+//Allowed options for action are: click, visit
+
+/*Interaction coding is as follows
+* Three-Five letter acronym that includes the category, object, action and if destination is present include in acronym
+* For example, category: 'button', object: 'image', action: 'click', data: {destination: 'Tumblr'} would have an acronym of
+*   BICT ('B' for button, 'I' for image, 'C' for click, 'T' for tumblr
+* */
+
 //Define interaction custom data below.
 manifest = {
-  "BLC": {category: "button", object: "hello_link", action: "click", data: {destination: 'yourFace'}},
-  "PGV": {category: "page", object: "gallery", action: "visit", data: {}}
+  "BLC": { category: "button", object: "hello_link", action: "click", data: {destination: 'yourFace'} },
+  "PGV": { category: "page", object: "gallery", action: "visit", data: {} }
 };
