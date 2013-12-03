b988dc51f1b740edb671fa1fdb67da46d19ac278
milyoni <mdevs@milyoni.com>
2013-12-03 11:15:17 -0800

    add demographic data for users

--- models/user.js
+++ models/user.js
@@ -65,47 +65,59 @@ exports.user = {
     //user demographics
     request('https://graph.facebook.com/fql?q=select+name,sex,birthday_date,hometown_location,current_location,friend_count,education,work+from+user+where+uid=' + data.suid + '&access_token=' + data.uat, function (err, res, body) {
       var demographics = JSON.parse(body);
-      var education = {};
-      var work = {};
 
       $.each(demographics.data, function (rowNumber, dataHash) {
-        //Education
-        $.each(dataHash.education, function (rowNumber, result) {
-          education[result.type.toLowerCase().replace(' ', '_')] = {
-            'name': result.school.name,
-            'year': result.year.name,
-            'page_id': result.school.id
-          };
-        });
-        //Work
+
+        //If education is present, add to education table
+        if (dataHash.education) {
+          self.education(dataHash.education, user_id);
+        }
+        //If work is present, add to work table
         if (dataHash.work) {
-          $.each(dataHash.work, function (rownumber, result) {
-            work["employer"] = {
-              'name': result.employer && result.employer.name,
-              'location': result.location && result.location.name,
-              'position': result.position && result.position.name,
-              'page_id': result.employer && result.employer.id
-            };
-          });
+          self.work(dataHash.work, user_id);
         }
       });
 
-      self.education(education, user_id);
-      self.work(work, user_id);
+      //Update user row with demographics data
+      self.demographics(demographics.data[0], user_id);
+    });
+  },
 
-      var details = demographics.data[0];
-      var location = hstore.stringify(details.current_location)
-//      pgQuery("UPDATE users SET location = hstore('" + location + "') WHERE id=" + user_id + ";");
-      console.info("UPDATE users SET location= hstore('" + location + "'), birthdate=" + details.birthday_date + ", gender='" + details.sex + "', source='facebook' " +
-        "WHERE id=" + user_id + ";")
+  demographics: function (data, user_id) {
+    var currentLocation = hstore.stringify(data.current_location);
+    var hometownLocation = hstore.stringify(data.hometown_location);
+    var birthday = new Date(data.birthday_date);
+    var formattedBirthday = birthday.getFullYear() + '-' + (birthday.getMonth() + 1) + '-' + birthday.getDate();
 
-      pgQuery("UPDATE users SET location= hstore('" + location + "'), birthdate=" + details.birthday_date + ", gender='" + details.sex + "', source='facebook' " +
-        "WHERE id=" + user_id + ";");
+    pgQuery("SELECT current_location ->'id' as id from users where id=" + user_id, function (res, err) {
+      if (err) {
+        return console.error("issue querying user and current location - user", err)
+      }
+      //if current_location page_id matches then don't update information
+      if ((res && !!(res.rows[0] && res.rows[0].id) && res.rows[0].id == data.current_location.id )) {
+        return true;
+      } else {
+        pgQuery("UPDATE users SET hometown_location= hstore('" + hometownLocation + "'), " +
+          "current_location= hstore('" + currentLocation + "'), " +
+          "birthdate=(to_date('" + formattedBirthday + "', 'YYYY-MM-DD')), " +
+          "gender='" + data.sex + "', source='facebook' " +
+          "WHERE id=" + user_id + ";");
+      }
     });
   },
 
   education: function (data, user_id) {
-    $.each(data, function (id, value) {
+    var education = {};
+    //Education
+    $.each(data, function (rowNumber, result) {
+      education[result.type.toLowerCase().replace(' ', '_')] = {
+        'name': result.school.name,
+        'year': result.year.name,
+        'page_id': result.school.id
+      };
+    });
+
+    $.each(education, function (id, value) {
       pgQuery("SELECT id from education where user_id=" + user_id + " AND page_id=" + value.page_id + ";", function (res, err) {
         if (err) {
           return console.error("issue querying user and page_id - education", err)
@@ -113,15 +125,27 @@ exports.user = {
         if (res && !!(res.rows[0] && res.rows[0].id)) {
           return true;
         } else {
-          pgQuery("INSERT into education (user_id, type, page_id, name, year) " +
-            "VALUES (" + user_id + ",'" + id + "'," + value.page_id + ",'" + value.name + "'," + parseInt(value.year) + ")");
+          pgQuery("INSERT into education (user_id, type, page_id, name, year, created_at) " +
+            "VALUES (" + user_id + ",'" + id + "'," + value.page_id + ",'" + value.name + "'," + parseInt(value.year) + ",'" + new Date().toISOString() + "');");
         }
       });
     })
   },
 
   work: function (data, user_id) {
-    $.each(data, function (id, value) {
+    var work = {};
+
+    //Work
+    $.each(data, function (rownumber, result) {
+      work["employer"] = {
+        'name': result.employer && result.employer.name,
+        'location': result.location && result.location.name,
+        'position': result.position && result.position.name,
+        'page_id': result.employer && result.employer.id
+      };
+    });
+
+    $.each(work, function (id, value) {
       pgQuery("SELECT id from work where user_id=" + user_id + " AND page_id=" + value.page_id + ";", function (res, err) {
         if (err) {
           return console.error("issue querying user and page_id - work", err)
@@ -129,38 +153,10 @@ exports.user = {
         if (res && !!(res.rows[0] && res.rows[0].id)) {
           return true;
         } else {
-          pgQuery("INSERT into work (user_id, location, position, page_id, company) " +
-            "VALUES (" + user_id + ",'" + value.location + "','" + value.position + "'," + value.page_id + ",'" + value.name + "')");
+          pgQuery("INSERT into work (user_id, location, position, page_id, company, created_at) " +
+            "VALUES (" + user_id + ",'" + value.location + "','" + value.position + "'," + value.page_id + ",'" + value.name + "','" + new Date().toISOString() + "');");
         }
       });
     })
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
